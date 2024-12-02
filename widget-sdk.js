25be8cff37590f2bfbe29ded42c3e8f07460fc9d
Seppe De Langhe <seppe.delanghe@intelliprove.com>
2024-12-02 11:00:15 +0100

    🏷️ tag version 1.0.2

--- widget-sdk.js
+++ widget-sdk.js
@@ -1,7 +1,7 @@
 /*
 	* @file Contains all the main code to manage intelli widgets in a website
 	* @author Seppe De Langhe <seppe.delanghe@intelliprove.com>
-	* @version 1.0.2-dev
+	* @version 1.0.2
 */
 
 class IntelliAuthError extends Error {
