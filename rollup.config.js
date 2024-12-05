2f960bc6803572248a78fca02dd7a6609591bd03
Seppe De Langhe <seppe.delanghe@intelliprove.com>
2024-12-05 15:00:09 +0100

    📝 replace rollup-plugin-terser with @rollup/plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import { babel } from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 const extensions = ['.js', '.ts'];
 const format = process.env.FORMAT;
